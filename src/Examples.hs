f579c8b9409690a1ae9667548bcd8ce5b0a48438
vitold <jahalive@gmail.com>
2018-01-17 00:49:58 +0100

    added imports and type annotations

--- src/Examples.hs
+++ src/Examples.hs
@@ -3,6 +3,11 @@
 
 module Examples where
 
+import           Control.Lens
+import           Control.Lens.Fold
+import           Control.Lens.Setter
+import           Control.Monad.Trans.State
+
 data Nested2 = Nested2
   { _field5::Int
   } deriving (Show)
@@ -42,15 +47,29 @@ initial = TopLevel
     ]
   }
 
---View field5 from nested lists
+--View field5 from nested lists:
+--λ> :t toListOf
+--  toListOf :: Getting (Data.Monoid.Endo [a]) s a -> s -> [a]
+--λ> :t traverse
+--  traverse
+--    :: (Applicative f, Traversable t) => (a -> f b) -> t a -> f (t b)
 viewFields5 = toListOf (field2 . traverse . field4 . traverse . field5) initial
 
 --Update fields in nested lists based on predicate
+--λ> :t execState
+--  execState :: State s a -> s -> s
+--λ> :t traversed
+--traversed
+--  :: (Applicative f2, Traversable f1, Indexable Int p) =>
+--     p a (f2 b) -> f1 a -> f2 (f1 b)
+--λ> :t filtered
+--filtered
+--  :: (Applicative f, Choice p) => (a -> Bool) -> Optic' p f a a
 updateFields5 = execState (field2 . traverse . field4 . traversed . filtered greaterThen2 . field5 -= 1) initial
 
 --Predicate for selecting elements to update
 greaterThen2 x =  x ^. field5 > 2
 
 --View nested arrays elements filtered by predicate
-filterField2 = toListOf (field2 . traverse . field4 . filtered greaterThen2) initial
+filterField2 = toListOf (field2 . traverse . field4 . traversed . filtered greaterThen2) initial
 
